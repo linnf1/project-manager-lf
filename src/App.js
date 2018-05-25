572c61097251081b14168458cf6577eec1f6b567
linnf1 <linnf@live.com>
2018-05-25 16:16:09 -0700

    Foundation setup for CRUD form

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({
+      projects: [
+        {
+          title: "Biz Site",
+          category: 'Web Design'
+        },
+        {
+          title: "Social Site",
+          category: 'Mobile Development'
+        },
+        {
+          title: "eCommerce shopping cart",
+          category: 'Web Development'
+        },
+      ]
+    })
+  }
+
+  handleAddProject(project){
+    console.log(project);
+  }
+
+  render() {
+     const { projects } = this.state;
+    return (
+      <div className="">
+        <AddProject addProject={this.handleAddProject.bind(this)} />
+        <Projects projects={ projects } />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/AddProject.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
+
+class AddProject extends Component {
+  constructor(){
+    super();
+    this.state = {
+      newProject:{}
+    }
+  }
+  static defaultProps = {
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+handleSubmit(e){
+  if(this.refs.title.value === ''){
+    alert("Cannot be empty");
+  }else {
+    this.setState({newProject:{
+      title: this.refs.title.value,
+      category: this.refs.category.value
+    }}, function(){
+      this.props.addProject(this.state.newProject);
+    });
+  }
+  e.preventDefault();
+}
+
+  render() {
+    let categoryOpts = this.props.categories.map(category => {
+      return <option key={category} value="category">{category}</option>
+    });
+    return (
+      <div className="">
+        <h3>Add Project</h3>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+          <label>Title</label>
+          <input type="text" ref="title" />
+          </div>
+          <div>
+          <label>category</label>
+          <select ref="category">
+          {categoryOpts}
+          </select>
+          </div>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
